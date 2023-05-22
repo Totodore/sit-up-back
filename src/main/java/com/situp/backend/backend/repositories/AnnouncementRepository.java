4e292971807850d59aef554e74076a12b8ee5c2f
Théodore Prévot <prevottheodore@gmail.com>
2023-05-22 17:16:05 +0200

    Fix: UserRepository

--- src/main/java/com/situp/backend/backend/repositories/AnnouncementRepository.java
+++ src/main/java/com/situp/backend/backend/repositories/AnnouncementRepository.java
@@ -1,8 +1,9 @@
 package com.situp.backend.backend.repositories;
 
+import com.situp.backend.backend.database.Announcement;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnnouncementRepository extends CrudRepository<AnnouncementRepository, Long> {
+public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
 }
