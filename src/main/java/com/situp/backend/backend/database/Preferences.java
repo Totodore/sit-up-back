031c97dd5a89eb17b7eeb65826dc4710575477f4
Marie SOL <marie.dol2001@gmail.com>
2023-05-30 17:56:52 +0200

    add preferences

---
+++ src/main/java/com/situp/backend/backend/database/Preferences.java
@@ -0,0 +1,53 @@
+package com.situp.backend.backend.database;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+public class Preferences {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private boolean cat;
+    private boolean dog;
+    private boolean frog;
+    private boolean rabbit;
+    private boolean turtel;
+    private boolean fish;
+    private boolean snake;
+    private boolean bird;
+    private boolean hamster;
+
+    private boolean paris;
+    private boolean marseille;
+    private boolean lyon;
+    private boolean toulouse;
+    private boolean nice;
+    private boolean nantes;
+    private boolean montpellier;
+    private boolean strasbourg;
+
+    private boolean petsitting;
+    private boolean plantsitting;
+    private boolean housesitting;
+
+    private boolean house;
+    private boolean appartment;
+
+    private boolean children;
+    private boolean otherPet;
+    private boolean smoke;
+}
+
