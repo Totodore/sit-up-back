375f975b8d78bb0dfa5bda5160f71a61f4097ea7
Bejo62192 <130389920+Bejo62192@users.noreply.github.com>
2023-06-12 20:00:28 +0200

    ajout procédure filtrante

--- src/main/java/com/situp/backend/backend/repositories/AnnouncementRepository.java
+++ src/main/java/com/situp/backend/backend/repositories/AnnouncementRepository.java
@@ -1,6 +1,7 @@
 package com.situp.backend.backend.repositories;
 
 import com.situp.backend.backend.database.Announcement;
+import com.situp.backend.backend.dto.FiltreDto;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -14,24 +15,13 @@ import java.util.List;
 public interface AnnouncementRepository extends CrudRepository<Announcement, Long> {
     @Query(value = "SELECT * FROM Announcement WHERE 1=1" +
             " AND (:city IS NULL OR city = :city)" +
-            " AND (:postalcode = 0 OR postalcode = :postalcode)" +
-            " AND (:description IS NULL OR description = :description)" +
-            " AND (:numberOfBeds = 0 OR numberOfBeds = :numberOfBeds)" +
-            " AND (:squareMeters = 0 OR squareMeters = :squareMeters)" +
+            " AND (:postalcode IS NULL OR postalcode = :postalcode)" +
+            " AND (:numberOfBeds IS NULL  OR numberOfBeds = :numberOfBeds)" +
+            " AND (:squareMeters IS NULL  OR squareMeters = :squareMeters)" +
             " AND (:startDate IS NULL OR startDate = :startDate)" +
             " AND (:stopDate IS NULL OR stopDate = :stopDate)" +
-            " AND (:numberPeopleMax = 0 OR numberPeopleMax = :numberPeopleMax)" +
+            " AND (:numberPeopleMax IS NULL  OR numberPeopleMax = :numberPeopleMax)" +
             " AND (:numberOfRooms = 0 OR numberOfRooms = :numberOfRooms)",
             nativeQuery = true)
-    List<Announcement> findFilteredAnnouncements(
-            @Param("city") String city,
-            @Param("postalcode") int postalcode,
-            @Param("description") String description,
-            @Param("numberOfBeds") int numberOfBeds,
-            @Param("squareMeters") int squareMeters,
-            @Param("startDate") Date startDate,
-            @Param("stopDate") Date stopDate,
-            @Param("numberPeopleMax") int numberPeopleMax,
-            @Param("numberOfRooms") int numberOfRooms);
-
+    List<Announcement> findFilteredAnnouncements( FiltreDto filtre);
 }
